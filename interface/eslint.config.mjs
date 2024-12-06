3b86b8127f22624d20f268629c6b0183792095ac
Aro Rahari <raha.aro@gmail.com>
2024-12-06 12:11:12 +0100

    udpate eslint

--- interface/eslint.config.mjs
+++ interface/eslint.config.mjs
@@ -2,11 +2,14 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      ...pluginJs.configs.recommended.rules, // Use recommended JavaScript rules
+      ...pluginReact.configs.flat.recommended.rules, // Use recommended React rules
+    },
+  },
 ];
